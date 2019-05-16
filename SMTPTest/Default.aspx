d09a862d546a3cd241dd9aa85ab5b862a9dbea76
riazpanjwani <49402838+riazpanjwani@users.noreply.github.com>
2019-05-16 14:51:24 -0400

    Add project files.

---
+++ SMTPTest/Default.aspx
@@ -0,0 +1,67 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="SMTPTest.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            
+<table style="font-family: Arial,'Segoe UI';">
+                <tr>
+                    <td colspan="3">
+                        <span align="left">
+                            <asp:Image ID="imgMail" runat="server" ImageUrl="~/Image/hp.png" />
+                        </span>
+                        <span style="font-size: 20px; font-family:"sans-serif"; margin-top: -5px">Sending Email through SMTP IIS
+                        </span>
+                    </td>
+                </tr>
+                <tr>
+                    <td>From</td>
+                    <td>:</td>
+                    <td>
+                        <asp:TextBox ID="txtFrom" runat="server" Width="407" Height="20"></asp:TextBox>
+                    </td>
+                </tr>
+                <tr>
+                    <td>To</td>
+                    <td>:</td>
+                    <td>
+                        <asp:TextBox ID="txtTo" runat="server" Width="407" Height="20"></asp:TextBox>
+                    </td>
+                </tr>
+                <tr>
+                    <td>Subject</td>
+                    <td>:</td>
+                    <td>
+                        <asp:TextBox ID="txtSubject" runat="server" Width="407" Height="20"></asp:TextBox>
+                    </td>
+                </tr>
+                <tr>
+                    <td>Message</td>
+                    <td>:</td>
+                    <td>
+                        <asp:TextBox ID="txtMessage" runat="server" TextMode="MultiLine" Rows="5" Columns="54"></asp:TextBox>
+                    </td>
+                </tr>
+                <tr>
+                    <td colspan="3" align="Right">
+                        <asp:Button ID="btnSend" runat="server" Text="Send mail" OnClick="btnSend_Click" BackColor="cornflowerblue" ForeColor="White" 
+                             Height="40" Width="200" Font-Size="18" BorderStyle="none" font-family="sans-serif"/>
+                    </td>
+                </tr>
+                <tr>
+                    <td colspan="3">
+                        <asp:Label ID="lblStatus" runat="server" />
+                    </td>
+                </tr>
+            </table>
+
+        </div>
+    </form>
+</body>
+</html>
---
+++ SMTPTest/Default.aspx.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Net.Mail;
+
+namespace SMTPTest
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnSend_Click (object sender, EventArgs e)
+        {
+            try
+            {
+                string from = txtFrom.Text.Trim(); //how is this authenticated?
+                string to = txtTo.Text.Trim();
+                string subject = txtSubject.Text.Trim();
+                string message = txtMessage.Text.Trim();
+
+                SmtpClient objSmtpClient = new SmtpClient(); //initializes new instance of SmtpClient class using config file settings
+                objSmtpClient.Host = "localhost"; //also specified in web config file
+                objSmtpClient.Port = 25; //configure to same port in IIS
+                objSmtpClient.DeliveryMethod = SmtpDeliveryMethod.SpecifiedPickupDirectory; //.Network, .PickupDirectoryFromIis, .SpecifiedPickupDirectory
+                //objSmtpClient.EnableSsl = true;
+                objSmtpClient.Timeout = 100000;
+                System.Diagnostics.Debug.WriteLine("Smtp set up completed");
+                objSmtpClient.Send(from, to, subject, message);
+                //SmtpClient.Credentials needed? -> Gets/sets the credentials used to authenticate the sender
+                System.Diagnostics.Debug.WriteLine("Email sent successfully");
+                lblStatus.Text = "<b style='color:green'>Email has been successfully sent</b>";
+            }
+
+            catch (SmtpException ex)
+            {
+                lblStatus.Text = "<b style='color:red'>Exception caught: </b>" + ex.ToString();
+                System.Diagnostics.Debug.WriteLine("Exception: " + ex.ToString());
+            }
+
+        }
+
+    }
+}
+
+/*
+public static void CreateTestMessage4(string server)
+{
+    MailAddress from = new MailAddress("ben@contoso.com");
+    MailAddress to = new MailAddress("Jane@contoso.com");
+    MailMessage message = new MailMessage(from, to);
+    message.Subject = "Using the SmtpClient class.";
+    message.Body = @"Using this feature, you can send an email message from an application very easily.";
+    SmtpClient client = new SmtpClient(server);
+    Console.WriteLine("Sending an email message to {0} by using SMTP host {1} port {2}.",
+    to.ToString(), client.Host, client.Port);
+    try
+    {
+        client.Send(message);
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine("Exception caught in CreateTestMessage4(): {0}",
+        ex.ToString());
+    }
+}
+}*/
+
